d3272ddf59446ad7f253d0c1518ff68a96255c86
ericknovais <erick.hora@outlook.com>
2024-04-30 20:53:52 -0300

    Interfaces: IContatoRepository IEnderecoRepository ITipoContatoRepository

--- GeraDados/GeraDados.DataModel/repository/IContatoRepository.cs
+++ GeraDados/GeraDados.DataModel/repository/IContatoRepository.cs
@@ -4,4 +4,5 @@ namespace GeraDados.DataModel.repository;
 
 public interface IContatoRepository:IRepositoryBase<Contato>
 {
+    IList<Contato> ObtemContatosPorIdPessoa(int IdPessoa);
 }
---
+++ GeraDados/GeraDados.DataModel/repository/IEnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeraDados.DataModel.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeraDados.DataModel.repository
+{
+    public interface IEnderecoRepository: IRepositoryBase<Endereco>
+    {
+        Endereco ObtemEnderecoPorIdPessoa(int idPessoa);
+    }
+}
