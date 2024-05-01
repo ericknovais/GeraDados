5035d697fd3bb076228d17e58951e927b2874a35
ericknovais <erick.hora@outlook.com>
2024-05-01 15:47:57 -0300

    Criação do formulario de upload

--- GeraDados/GeraDados.App/frmUpload.cs
+++ GeraDados/GeraDados.App/frmUpload.cs
@@ -1,8 +1,8 @@
 namespace GeraDados.App
 {
-    public partial class Form1 : Form
+    public partial class frmUpload : Form
     {
-        public Form1()
+        public frmUpload()
         {
             InitializeComponent();
         }
