d5b6382631645510f85cd13a378aba544ebc1e4a
ericknovais <erick.hora@outlook.com>
2024-05-01 16:19:21 -0300

    Inicio do metodo salvar pessoas

--- GeraDados/GeraDados.App/frmUpload.cs
+++ GeraDados/GeraDados.App/frmUpload.cs
@@ -1,41 +1,52 @@
+using Geradados.DataAccess.DB.Dtos;
 using Geradados.DataAccess.Repository;
 using GeraDados.DataModel.models;
+using Newtonsoft.Json;
 
-namespace GeraDados.App
+
+namespace GeraDados.App;
+
+public partial class frmUpload : Form
 {
-    public partial class frmUpload : Form
+    Repository repository = new Repository();
+
+    public frmUpload()
     {
-        Repository repository = new Repository();
+        InitializeComponent();
+        if (repository.TipoContato.ObterTodos().Count.Equals(0))
+            CarregaTipoDeContatos();
+    }
 
-        public frmUpload()
-        {
-            InitializeComponent();
-            if (repository.TipoContato.ObterTodos().Count.Equals(0))
-                CarregaTipoDeContatos();
-        }
+    private void CarregaTipoDeContatos()
+    {
+        var listadeTipocontatos = new TipoContato().CarregaListaTipoContato();
+        foreach (var tipoContato in listadeTipocontatos)
+            repository.TipoContato.Salvar(tipoContato);
 
-        private void CarregaTipoDeContatos()
+        repository.SaveChanges();
+    }
+
+    private void btnUpload_Click(object sender, EventArgs e)
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        ofd.CheckFileExists = true;
+        ofd.Multiselect = false;
+        if (ofd.ShowDialog() == DialogResult.OK)
+            txtArquivo.Text = ofd.FileName;
+    }
+
+    private void btnSalvar_Click(object sender, EventArgs e)
+    {
+        StreamReader reader = new StreamReader(txtArquivo.Text);
+        string json = reader.ReadToEnd();
+        IList<PessoaJson> pessoas = JsonConvert.DeserializeObject<IList<PessoaJson>>(json);
+
+        foreach (var item in pessoas)
         {
-            var listadeTipocontatos = new TipoContato().CarregaListaTipoContato();
-            foreach (var tipoContato in listadeTipocontatos)
+            Pessoa pessoa = new Pessoa()
             {
-                repository.TipoContato.Salvar(tipoContato);
-                repository.SaveChanges();
-            }
-        }
-
-        private void btnUpload_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.CheckFileExists = true;
-            ofd.Multiselect = false;
-            if (ofd.ShowDialog() == DialogResult.OK)
-                txtArquivo.Text = ofd.FileName;
-        }
-
-        private void btnSalvar_Click(object sender, EventArgs e)
-        {
-
+                CPF = item.CPF,
+            };
         }
     }
 }
---
+++ GeraDados/Geradados.DataAccess/DB/Dtos/PessoaJson.cs
@@ -0,0 +1,20 @@
+﻿namespace Geradados.DataAccess.DB.Dtos;
+
+public class PessoaJson
+{
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public string RG { get; set; }
+    public DateTime Data_nasc { get; set; }
+    public string Email { get; set; }
+    public string CEP { get; set; }
+    public string Endereco { get; set; }
+    public int Numero { get; set; }
+    public string Bairro { get; set; }
+    public string Cidade { get; set; }
+    public string Estado { get; set; }
+    public string Telefone_fixo { get; set; }
+    public string Celular { get; set; }
+
+
+}
